aa506881a6d03a8929339e3f0a78069e0fda928b
Gantcho Kojuharov <gantcho@greyarch.net>
2016-11-17 15:54:10 +0100

    full impl

--- index.js
+++ index.js
@@ -2,75 +2,81 @@
 'use strict';
 
 const fs = require('fs');
+const path = require('path');
 const spawn = require('child_process').spawn;
 const parser = require("nomnom");
-const yaml = require("js-yaml");
-
-var defaults = {};
-if (fs.existsSync('.indocker')) {
-  defaults = yaml.safeLoad(fs.readFileSync('.indocker', 'utf8')) || {};
-} else {};
 
 const options = parser
   .option('image', {
     abbr: 'i',
     help: 'use this docker image',
-    default: defaults.image || 'alpine'
+    default: 'alpine'
+  })
+  .option('entrypoint', {
+    abbr: 'e',
+    help: 'set this as a docker run entrypoint'
   })
   .option('command', {
     abbr: 'c',
-    help: 'execute this command passign the target file as an argument',
-    default: defaults.command || 'sh'
+    help: 'set this as a docker run command'
   })
   .option('dir', {
     abbr: 'd',
-    help: 'input dir; will be mappped to /.indocker/input inside the container, defaults to the current folder',
-    default: defaults.dir || '$(pwd)'
+    help: 'input dir',
+    default: '.'
   })
   .option('out', {
     abbr: 'o',
-    help: 'output dir; will be mappped to /.indocker/output inside the container, defaults to the current folder',
-    default: defaults.out || '$(pwd)'
+    help: 'output dir',
+    default: '.'
   })
   .option('user', {
     abbr: 'u',
-    help: 'execute as this user:group',
-    default: defaults.user || '`id -u`:`id -g`'
+    help: 'execute as this user[:group]',
+    default: '`id -u`:`id -g`'
   })
   .parse();
 
 
 const cmd = 'docker';
-const args = [
-  'run',
-  '--rm',
-  '-v', `${options.dir}:/.indocker/input`,
-  '-v', `${options.out}:/.indocker/output`,
-  '-w', `/.indocker/input`,
+const indir = path.resolve(options.dir);
+const outdir = path.resolve(options.out);
+var args = ['run', '--rm'];
+if(options.entrypoint) {
+  args = args.concat(['--entrypoint', options.entrypoint])
+};
+args = args.concat([
+  '-v', `${indir}:${indir}`,
+  '-v', `${outdir}:${outdir}`,
+  '-w', indir,
   '-u', options.user,
-  options.image,
-  options.command,
-  options._[0]];
+  options.image]);
+if(options.command) {args.push(options.command)};
+args = args.concat(options._);
 const opts = {
   shell: true
 };
 
-console.log('indocker is starting', defaults, options, args);
+console.log('indocker is starting', options, args);
 
 const ind = spawn(cmd, args, opts)
 
 ind.stdout.on('data', (data) => {
-  console.log(`stdout: ${data}`);
+  console.log(`${data}`);
 });
 
 ind.stderr.on('data', (data) => {
-  console.error(`stderr: ${data}`);
+  console.error(`${data}`);
 });
 
 ind.on('close', (code) => {
-  console.log(`child process exited with code ${code}`);
+  if(code) {
+    console.error(`Child process exited with code ${code}`);
+    process.exit(code);
+  }
 });
 
 ind.on('error', (err) => {
   console.error('Failed to start child process.');
+  process.exit(1);
 });
--- script.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-cp index.js ../output/
-ls -la ../output
