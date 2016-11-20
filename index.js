98fbbb9e46164a316eae12eb2e7863e3a51ddb43
Gantcho Kojuharov <gantcho@greyarch.net>
2016-11-20 11:10:11 +0100

    rewritten using commander and improved the api

--- index.js
+++ index.js
@@ -4,95 +4,72 @@
 const fs = require('fs');
 const path = require('path');
 const spawn = require('child_process').spawn;
-const parser = require("nomnom");
+const program = require('commander');
 
-const options = parser
-  .script('indocker')
-  .help('indocker [options] [commands to execute]')
-  .option('image', {
-    abbr: 'i',
-    help: 'use this docker image',
-    default: 'alpine'
-  })
-  .option('entrypoint', {
-    abbr: 'e',
-    help: 'will be used as a docker run entrypoint'
-  })
-  .option('command', {
-    abbr: 'c',
-    help: 'will be used as a docker run command'
-  })
-  .option('dir', {
-    abbr: 'd',
-    help: 'input dir',
-    default: '.'
-  })
-  .option('out', {
-    abbr: 'o',
-    help: 'output dir',
-    default: '.'
-  })
-  .option('user', {
-    abbr: 'u',
-    help: 'execute as this user[:group]',
-    default: '`id -u`:`id -g`'
-  })
-  .option('opts', {
-    help: 'additional docker options to be passed as docker run options'
-  })
-  .option('debug', {
-    flag: true,
-    hidden: true
-  })
-  .parse();
-
-
-const cmd = 'docker';
-const indir = path.resolve(options.dir);
-const outdir = path.resolve(options.out);
-var args = ['run', '--rm'];
-if(options.entrypoint) {
-  args = args.concat(['--entrypoint', options.entrypoint])
-};
-args = args.concat([
-  '-v', `${indir}:${indir}`,
-  '-v', `${outdir}:${outdir}`,
-  '-w', indir,
-  '-u', options.user,
-  options.opts || '',
-  options.image
-]);
-if(options.command) {args.push(options.command)};
-args = args.concat(options._);
-const opts = {
-  shell: true
-};
-
-if(options.debug) {
-  console.log('INDOCKER DEBUG:');
-  console.log(' OPTIONS:\n', options);
-  console.log();
-  console.log(' DOCKER ARGS:\n', args);
+function volumes(vol, memo) {
+  memo.push(vol);
+  return memo;
 }
 
-const ind = spawn(cmd, args, opts)
+program
+  .version(require('./package.json').version)
+  .usage('[options] "command arg1 arg2..."')
+  .option('-i, --image <image>', 'Will run the command in this image', 'alpine')
+  .option('-e, --entrypoint <executable>', 'Will be used to override the image entrypoint')
+  .option('-v, --volume <dir>', 'Volumes (directories and/or files) to mount inside the container. Can be specified multiple times. Current directory will always be mounted', volumes, ['.'])
+  .option('-u, --user <uid[:guid]>', 'Will run the command as this user[:group]', '`id -u`:`id -g`')
+  .option('-o, --opts <options>', 'Additional docker run options', '')
+  .option('--debug', 'Run in debug mode')
+  .arguments('<cmd>')
+  .action(function (command, options) {
+    const cmd = 'docker';
+    var args = ['run', '--rm'];
+    if(options.entrypoint) {
+      args = args.concat(['--entrypoint', options.entrypoint])
+    };
+    options.volume.map(vol => {
+      const v = path.resolve(vol);
+      args.push('-v');
+      args.push(`${v}:${v}`);
+    });
+    args = args.concat([
+      '-w', process.cwd(),
+      '-u', options.user,
+      options.opts,
+      options.image
+    ]);
+    args = args.concat(command);
+    const opts = {
+      shell: true
+    };
 
-ind.stdout.on('data', (data) => {
-  console.log(`${data}`);
-});
+    if(options.debug) {
+      console.log('INDOCKER DEBUG:');
+      console.log(' OPTIONS:\n', args);
+      console.log();
+    }
 
-ind.stderr.on('data', (data) => {
-  console.error(`${data}`);
-});
+    const ind = spawn(cmd, args, opts)
 
-ind.on('close', (code) => {
-  if(code) {
-    console.error(`Child process exited with code ${code}`);
-    process.exit(code);
-  }
-});
+    ind.stdout.on('data', (data) => {
+      console.log(`${data}`);
+    });
 
-ind.on('error', (err) => {
-  console.error('Failed to start child process.');
-  process.exit(1);
-});
+    ind.stderr.on('data', (data) => {
+      console.error(`${data}`);
+    });
+
+    ind.on('close', (code) => {
+      if(code) {
+        console.error(`Child process exited with code ${code}`);
+        process.exit(code);
+      }
+    });
+
+    ind.on('error', (err) => {
+      console.error('Failed to start child process.');
+      process.exit(1);
+    });
+  });
+
+program.parse(process.argv);
